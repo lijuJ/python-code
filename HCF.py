c4d2df4de7b8968315c6048e8d56a9b0b463e16a
liju <liju7728@gmail.com>
2023-08-31 08:57:17 +0530

    HCF and LCM added

---
+++ HCF.py
@@ -0,0 +1,11 @@
+def hcf(a, b):
+    if b == 0:
+        return a
+    else:
+        return hcf(b, a % b)
+
+
+first = 23
+second = 69
+
+print('HCF of', first, 'and', second, 'is', hcf(first, second))
